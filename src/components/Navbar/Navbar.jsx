dff46972f7394a2783b7c4bf1ebda047e162cc19
Adithyavarun-creator <74570395+Adithyavarun-creator@users.noreply.github.com>
2024-02-11 19:45:52 +0530

    mob push

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,18 +2,28 @@ import React from "react";
 import "./Navbar.css";
 import WhiteLogo from "../../assets/blacklogo.png";
 import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   return (
+    <>
     <nav className="nav-container">
       <Link to="/" className="linkstyle nav-img-box">
         <img src={WhiteLogo} className="vividlylogo" alt="" />
       </Link>
       <div className="nav-link-box">
         <li className="nav-link">L/D mode</li>
-        <li className="nav-link">Pricing</li>
+        <li className="nav-link">About</li>
       </div>
     </nav>
+
+
+    <nav className="mobile-navsection">
+      <div className="mobile-menubox">
+        <GiHamburgerMenu className="mobile-menuicon"/>
+      </div>
+    </nav>
+    </>
   );
 };
 
